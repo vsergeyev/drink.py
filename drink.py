f9c05b44246e6b45d4a174056b7d737066ad9b43
Volodymyr Sergeyev <pydevside@gmail.com>
2012-02-03 20:10:29 +0200

    Basic classes

---
+++ drink.py
@@ -0,0 +1,18 @@
+class Beer:
+    """Generic beer"""
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Beer"
+
+
+class Vodka:
+    """Generic vodka"""
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "Vodka"
